13957e838b8662fe4fcf2cbab1a151d4fbc43edb
Legacy35 <44005399+Legacy35@users.noreply.github.com>
2021-04-07 16:16:08 -0400

    Updated Data Format

--- Data Ingestion.py
+++ Data Ingestion.py
@@ -23,9 +23,9 @@ if subimission_comment_ids:
     subimission_comment_ids = subimission_comment_ids.rstrip(subimission_comment_ids[-1])
     response = requests.get("https://api.pushshift.io/reddit/search/comment/?ids="+subimission_comment_ids)
  
-    row_list = [["Id","Date_Created_Utc","Score","Parent_id","Body","Link"]]
+    row_list = [["Id","Date_Created_Utc","Score","Parent_id","Body","Link", "Mentioned Nouns", "Sentiment"]]
     for x in response.json()["data"]:
-        row_list.append([x["id"],x["created_utc"],x["score"],x["parent_id"],x["body"],x["permalink"]])
+        row_list.append([x["id"],x["created_utc"],x["score"],x["parent_id"],x["body"],x["permalink"], "",""])
     row_list.append(["Total Comments:",len(row_list), "Total Posts:", count])
     with open(subreddit+'.csv', 'w', newline='', encoding="utf-8") as file:
         writer = csv.writer(file)
