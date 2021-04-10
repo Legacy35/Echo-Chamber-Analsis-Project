b3cb7b23739c5d95b231da06c9c596e78a758cd5
Matthew Parks <matthewparks373@gmail.com>
2021-04-10 02:41:45 -0400

    parse raw pushshift files into csv

--- pushshift_file_parsing/json_to_csv.sh
+++ pushshift_file_parsing/json_to_csv.sh
@@ -1,19 +1,21 @@
 #!/bin/bash
+# Instal ripgrep and jq
 # Take in Pushshift raw files and output them into a CSV file
 FILENAME=RC_2016-11
 
 # Comment to unpack
-bunzip2 -f -k -v $FILENAME.bz2
-mv -f $FILENAME $FILENAME.json
+#bunzip2 -f -k -v $FILENAME.bz2
+# mv -f $FILENAME $FILENAME.json
 
-subreddits=("conservative" "socialism" "politics" "republican" "news" "lateStageCapitalism")
+subreddits=("The_Donald" "LateStageCapitalism" "Republican" "Republican" "republicans" "Democrat" "democrats" "socialism" "politics" "news")
 for i in "${subreddits[@]}"
 do
-  echo "Parsing out \"subreddit\":\"$i\""
-  grep "\"subreddit\":\"$i\"" $FILENAME.json > "${FILENAME}_$i.json"
-  # head -n 1000 FILENAME.json | grep "\"subreddit\":\"$i\"" > "${FILENAME}_$i.json"
   echo "Formatting subreddit:$i from json into csv"
-  jq --raw-output '. | [.name, .parent_id, .link_id, .author,.created_utc, .score, .body] | @csv' "${FILENAME}_$i.json" > "${FILENAME}_$i.csv"
+  # This is brutally slow, use grep to filter out strings instead
+  #jq ". |
+  #    select( .subreddit == \"$i\" ) |
+  #    [.name, .parent_id, .subreddit, .link_id, .author,.created_utc, .score, .body] | @csv" "${FILENAME}.json" > "${FILENAME}_$i.csv"
+  rg --ignore-case "\"subreddit\":\"$i\"" "${FILENAME}.json" | jq ". | [.id, .name, .parent_id, .subreddit, .link_id, .author,.created_utc, .score, .body] | @csv" > "${FILENAME}_$i.csv"
 done
 
-7za a ${FILENAME}_csv.7z ${FILENAME}_*.csv
+7za a ${FILENAME}_subreddits.7z ${FILENAME}_*.csv
